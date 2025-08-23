f58f3d8fae29afdb7892815b325d2b9f760fb7b1
Ubuntu <ubuntu@ip-172-31-44-47.eu-north-1.compute.internal>
2025-08-23 22:30:29 +0000

    Save local changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,11 @@
 
+
 //production
 //export const BASE_URL = "/api"; 
 
 // dev
 export const BASE_URL =
  location.hostname==="localhost" ?"http://localhost:5000" : "/api";
+
+export const BASE_URL = "/api";
+
